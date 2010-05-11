0285dcdb1acb4a6377476f3320ceb42c0351d3d4
James Robertson <james@r0bertson.co.uk>
2010-05-11 12:18:53 +0100

    first commit

---
+++ lib/optparse-simple.rb
@@ -0,0 +1,104 @@
+require 'rexml/document'
+include REXML
+
+class OptParseSimple
+
+  def initialize(filename, args)
+    doc = Document.new(File.open(filename,'r').read)
+    @options = XPath.match(doc.root, 'records/option[summary/switch!=""]')
+
+    switches = @options.map do |option| 
+      switch = option.text('summary/switch')
+      switch[0] == '-' ? switch : nil
+    end
+
+    switches.compact!
+
+    # split the argument switches if grouped e.g. -ltr
+    args.map! do |arg|
+
+      if arg[/^\-[a-zA-Z]+$/] and switches.grep(/#{arg}/).empty? then
+        arg[1..-1].scan(/./).map {|x| '-' + x} 
+      else
+        arg
+      end
+    end
+
+    args.flatten!
+
+    a1 = options_match(@options[0], args).flatten.each_slice(2).map {|x| x if x[0]}.compact
+    options_remaining = XPath.match(doc.root, 'records/option/summary[switch=""]/name/text()')
+    a2 = args.zip(options_remaining).map(&:reverse)
+    if a2.map(&:first).all? then
+      @h = Hash[*(a1+a2).map{|x,y| [x.to_s.to_sym, y]}.flatten]
+    else
+      invalid_option = a2.detect {|x,y| x.nil? }.last
+      raise "invalid option: %s not recognised" % invalid_option
+    end
+  end
+
+  def to_h()
+    @h
+  end
+
+  private
+
+  def options_match(option, args)
+
+    switch, switch_alias = option.text('summary/switch'), option.text('summary/alias')
+    switch_pattern = switch_alias ? "(%s|%s)" % [switch, switch_alias] : switch 
+    switch_matched, arg_index = args.each_with_index.detect {|x,j| x[/^#{switch_pattern}/]}
+    key, value = nil
+
+    if switch_matched then
+
+      value_pattern = option.text('summary/value')
+
+      if value_pattern then
+
+        # check for equal sign
+        value = switch_matched[/\=(#{value_pattern})/,1]
+
+        # check the next arg
+        if value.nil? and args.length > 0 then
+
+          next_arg = args[arg_index + 1] 
+
+          # check to make sure it's not the next switch
+          next_option = @options[1] if @options.length > 1
+
+          if next_arg != next_option then
+            # validate using the regex
+            value = next_arg[/#{value_pattern}/]
+
+            if value then
+              args.delete_at(arg_index + 1)
+            else
+              raise option.text('records/error[last()]/summary/msg')
+            end
+          end
+
+        else
+          args.delete_at(arg_index)
+        end      
+
+      else
+        args.delete_at(arg_index)
+      end
+
+      key = option.text('summary/name')
+ 
+    elsif option.text('summary/mandatory').downcase == 'true' then
+
+      raise option.text('records/error/summary/msg')
+
+    end
+
+    pair = [key, value]
+    @options.shift
+    next_pair = options_match(@options[0], args) if @options.length > 0
+
+    [pair, next_pair]
+  end
+
+end
---
+++ optparse-simple.gemspec
@@ -0,0 +1,6 @@
+Gem::Specification.new do |s|
+  s.name = 'optparse-simple'
+  s.version = '0.2.0'
+  s.summary = 'optparse-simple'
+  s.files = Dir['lib/**/*.rb']
+end
