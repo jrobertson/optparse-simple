91e44998906b3a2074c5a81f2b39fbccf5e9e94a
James Robertson <james@r0bertson.co.uk>
2015-02-08 18:27:28 +0000

    bug fix: resorted back to using REXML until the issue with Rexle is resolved.

--- lib/optparse-simple.rb
+++ lib/optparse-simple.rb
@@ -9,26 +9,28 @@ require 'table-formatter'
 
 class OptParseSimple
   include REXML
-  
+
   def initialize(s)    
     super()
-    
-    buffer = read(s)    
-    @doc = Document.new(buffer)    
-    
+    buffer = readx(s)
+
+    @doc = Document.new(buffer)
+    #@doc = Rexle.new(buffer)
   end
   
   def parse(args)
-    
-    @options = XPath.match(@doc.root, 'records/optionx[summary/switch!="n/a"]')
 
-    switches = @options.map do |option| 
+    @options = XPath.match(@doc.root, 'records/optionx[summary/switch!="n/a"]')
+    #@options = @doc.root.xpath('records/optionx[summary/switch!="n/a"]')
+
+    switches = @options.map do |option|
+
       switch = option.text('summary/switch')
       switch[0] == '-' ? switch : nil
     end    
 
     switches.compact!
-
+    
     # split the argument switches if grouped e.g. -ltr
     args.map! do |arg|
 
@@ -44,6 +46,7 @@ class OptParseSimple
     # -- bind any loose value to their argument
     xpath = 'records/optionx/summary[switch != "n/a" and value != "n/a"]'
     options = XPath.match(@doc.root, xpath)\
+    #options = @doc.root.xpath(xpath)\
         .map {|node|  %w(switch alias)\
           .map{|x| node.text(x)}.compact}\
         .flatten
@@ -56,15 +59,23 @@ class OptParseSimple
     # -- end of bind any loose value to their argument
 
     a1 = []
+
     a1 = options_match(@options[0], args).flatten.each_slice(2).map {|x| x if x[0]}.compact unless @options.empty?
+
     options_remaining = XPath.match(@doc.root, 'records/optionx/summary[switch="n/a"]/name/text()').map(&:to_s)
+    #options_remaining = @doc.root.xpath('records/optionx/summary[switch="n/a"]/name/text()').map(&:to_s)
     mandatory_remaining  = XPath.match(@doc.root, 'records/optionx/summary[switch="n/a" and mandatory="true"]/name/text()').map(&:to_s)
+    #mandatory_remaining  = @doc.root.xpath('records/optionx/summary[switch="n/a" and mandatory="true"]/name/text()').map(&:to_s)
+
     if mandatory_remaining.length > args.length then
+
        missing_arg = (mandatory_remaining - args).first
        option = XPath.first(@doc.root, "records/optionx[summary/name='#{missing_arg}']")
-       raise option.text('records/errorx/summary/msg')
+       #option = @doc.root.element("records/optionx[summary/name='#{missing_arg}']")
+
+       raise option.text('records/errorx/summary/msg') || 'missing arg'
     end
-    
+
     a2 = args.zip(options_remaining).map(&:reverse)
     if a2.map(&:first).all? then
       @h = Hash[*(a1+a2).map{|x,y| [x.to_s.strip.to_sym, y]}.flatten]
@@ -72,6 +83,7 @@ class OptParseSimple
       invalid_option = a2.detect {|x,y| x.nil? }.last
       raise "invalid option: %s not recognised" % invalid_option
     end
+
     @h
   end
 
@@ -81,9 +93,10 @@ class OptParseSimple
   
   def help    
     a = XPath.match(@doc.root,  "records/optionx/summary[switch != 'n/a']").map do |summary|
+    #a = @doc.root.xpath("records/optionx/summary[switch != 'n/a']").map do |summary|
       %w(switch alias).map {|x| summary.text x}
     end
-    puts 'options:'
+
     puts TableFormatter.new(source: a, border: false).to_s
   end
 
@@ -114,6 +127,7 @@ class OptParseSimple
           next_option = @options[1] if @options.length > 1
 
           if next_arg != next_option then
+
             # validate using the regex
             value = next_arg[/#{value_pattern}/]
 
@@ -129,6 +143,7 @@ class OptParseSimple
         end      
 
       else
+
         args.delete_at(arg_index)
       end
 
@@ -148,8 +163,9 @@ class OptParseSimple
     [pair, next_pair]
   end
   
-  def read(s)
-    if s[/\s/] then
+  def readx(s)
+
+    r = if s[/\s/] then
       Polyrex.new('options/optionx[name,switch,alias,value,mandatory]/errorx[msg]').parse(s).to_xml
     elsif s.is_a? Polyrex then
       s.to_xml
@@ -158,8 +174,10 @@ class OptParseSimple
     elsif s[/\</] # xml
       s
     else # local file
-      File.open(s,'r').read
+      File.read s
     end    
+    
+    r
   end
 
 end
--- optparse-simple.gemspec
+++ optparse-simple.gemspec
@@ -1,17 +1,18 @@
 Gem::Specification.new do |s|
   s.name = 'optparse-simple'
-  s.version = '0.4.2'
+  s.version = '0.4.3'
   s.summary = 'optparse-simple'
   s.description = 'OptParse-Simple parses command-line arguments and returns them as a hash.'
   s.author = 'James Robertson'
   s.homepage = 'https://github.com/jrobertson/optparse-simple'
     s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
-  s.add_dependency('polyrex')
-  s.add_dependency('table-formatter') 
+  s.add_runtime_dependency('polyrex', '~> 0.9', '>=0.9.3')
+  s.add_runtime_dependency('table-formatter', '~> 0.1', '>=0.1.13') 
   s.signing_key = '../privatekeys/optparse-simple.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
   s.homepage = 'https://github.com/jrobertson/optparse-simple'
+  s.required_ruby_version = '>= 2.1.2'
 end
